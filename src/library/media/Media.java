b14125904f6042c399ea16ba4994a6676168fda5
Trolann <trevor@mathisen.dev>
2022-11-14 12:36:43 -0800

    checkedin attribute for Media

--- src/library/media/Media.java
+++ src/library/media/Media.java
@@ -6,17 +6,20 @@ public abstract class Media implements Comparable<Media> {
     public final String libraryFileName = "library.txt";
     protected String creator;
     protected String title;
+    protected boolean checkedIn;
 
     // Constructor - Media must be made with the Title and publishedDate in order to facilitate comparing
     // This constructor is called by each subclass's constructor, so setting creator and title is not needed.
-    public Media(String mediaTitle, String creator) {
+    public Media(String mediaTitle, String creator, boolean checkedIn) {
         this.creator = creator;
         this.title = mediaTitle;
+        this.checkedIn = checkedIn;
     }
 
     public Media() {
         this.creator = "";
         this.title = "";
+        this.checkedIn = false;
     }
 
     // Abstract methods
