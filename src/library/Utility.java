966a766ef30c644ede2042936409a956b942379f
Trolann <trevor@mathisen.dev>
2022-11-14 15:34:01 -0800

    listMedia complete

--- src/library/Utility.java
+++ src/library/Utility.java
@@ -7,15 +7,16 @@ import library.media.Video;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Utility {
     public static void main(String[] args) {
-        LinkedList<Media> mediaList = listMedia();
-        for(Media media : mediaList) {
-            System.out.println(media.toString());
-        }
+        HashMap<String, Media> mediaList = listMedia();
+        mediaList.forEach((key, value) -> {
+            System.out.println(value.toString());
+            });
     }
 
     // Filename (relative location) of the file with library information.
@@ -27,8 +28,8 @@ public class Utility {
     }
 
     // This function returns a LinkedList of Media objects for display in the GUI
-    public static LinkedList<Media> listMedia() {
-        LinkedList<Media> returnList = new LinkedList<Media>();
+    public static HashMap<String, Media> listMedia() {
+        HashMap<String, Media> returnList = new HashMap<String, Media>();
 
         File libraryFile = new File(library.Utility.getLibraryFileName());
         Scanner fileScanner = null; // Assigned to quiet down IDE warnings
@@ -48,7 +49,7 @@ public class Utility {
             while (lineScanner.hasNext()) {
                 // Get the next value, store in string to be safe
                 nextLine = lineScanner.next();
-                System.out.println(nextLine);
+                //System.out.println(nextLine);
                 String[] splitValues = nextLine.split(",");
 
 
@@ -56,8 +57,14 @@ public class Utility {
                 // and then casts the object to the Media type for display.
                 switch(splitValues[0]) {
                     // Starts with..  ..add to return ...and auto-cast to Media
-                    case "video" -> { returnList.add(new Video(nextLine)); }
-                    case "audiobook" -> { returnList.add(new AudioBook(nextLine)); }
+                    case "video" -> {
+                        Video o = new Video(nextLine);
+                        returnList.put(o.getTitle(), o);
+                    }
+                    case "audiobook" -> {
+                        AudioBook o = new AudioBook(nextLine);
+                        returnList.put(o.getTitle(), o);
+                    }
                     // Add additional Media objects below, update type and Type
                     // case "type" -> { returnList.add(new Type(nextLine)); }
                 }
