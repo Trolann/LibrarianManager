f529aea70c769aacdcef7661254bf468809cb632
Trolann <trevor@mathisen.dev>
2022-11-14 08:47:41 -0800

    added LibraryFunctions interface

---
+++ src/library/LibraryFunctions.java
@@ -0,0 +1,6 @@
+package library;
+
+public interface LibraryFunctions {
+    public boolean checkIn();
+    public boolean checkOut();
+}
--- src/library/roles/RoleActions.java
+++
@@ -1,8 +0,0 @@
-package library.roles;
-import library.media.Media;
-
-public interface RoleActions {
-    boolean checkIn(Media o);
-    boolean checkOut(Media o);
-    Media[] list(); // Returns media checked out by Role or available Media from Librarian
-}
