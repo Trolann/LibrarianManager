51e850d5b70e8b1cfec25c99435b1ea5695b0a0e
Trolann <trevor@mathisen.dev>
2022-11-15 14:46:01 -0800

    GUI v1 (mock-up)

---
+++ src/library/LibraryFrame.java
@@ -0,0 +1,19 @@
+package library;
+
+import javax.swing.*;
+
+public class LibraryFrame {
+    private JCheckBox typeAll;
+    private JCheckBox bookFilter;
+    private JCheckBox audioBooksFilter;
+    private JTextField searchTitle;
+    private JCheckBox newspaperFilter;
+    private JCheckBox eTextbookFilter;
+    private JCheckBox checkBox5;
+    private JCheckBox checkBox6;
+    private JComboBox comboBox1;
+    private JButton checkInButton;
+    private JButton checkOutButton;
+    private JButton button3;
+    private JLabel searchResultsLabel;
+}
